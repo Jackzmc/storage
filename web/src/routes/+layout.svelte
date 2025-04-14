6862d55206ee72665fddaef65d0cf1831b25c6e2
Jackz <me@jackz.me>
2025-04-14 18:20:10 -0500

    Work on initial markup of UI

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
 	import '../app.css';
+	import Nav from '$lib/components/nav.svelte';
+	import Sidebar from '$lib/components/sidebar.svelte'
 	
 	let { children } = $props();
 </script>
 
-{@render children()}
+<Nav />
+<div class="mx-5">
+	<div class="columns">
+		<div class="column pl-0">
+			<Sidebar />
+		</div>
+		<div class="column is-10">
+			{@render children()}
+		</div>
+	</div>
+</div>
