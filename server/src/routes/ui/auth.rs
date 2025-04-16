692aeb438334f1cb5759c83457505498c1a0e1d0
Jackz <me@jackz.me>
2025-04-15 20:33:06 -0500

    Add register page

--- server/src/routes/ui/auth.rs
+++ server/src/routes/ui/auth.rs
@@ -6,8 +6,21 @@ pub async fn login(route: &Route) -> Template {
     Template::render("auth/login", context! { route: route.uri.path() })
 
 }
+
 #[post("/login")]
 pub async fn login_handler(route: &Route) -> Template {
     Template::render("auth/login", context! { route: route.uri.path() })
+
+}
+
+#[get("/register")]
+pub async fn register(route: &Route) -> Template {
+    Template::render("auth/register", context! { route: route.uri.path() })
+
+}
+
+#[post("/register")]
+pub async fn register_handler(route: &Route) -> Template {
+    Template::render("auth/register", context! { route: route.uri.path() })
 
 }
